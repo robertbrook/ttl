7d246d317b484aaeb295d1a5f29ae41444dcde90
Glitch (ttl) <none>
2019-04-29 19:40:06 +0000

    👳🎙 Updated with Glitch

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.get('/render', function (req, res) {
   const store = new N3.Store();
   const ttlurl = req.query.url;
   
-  const myURL = url.parse('https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash');
-  console.log(myURL);
-  
   request(ttlurl, function (error, response, body) {       
     store.addQuads(parser.parse(body));
     
