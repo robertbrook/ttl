51e4ef51c7473bb344ee7e0631cd4948ec1a036e
Robert Brook <robertbrook@fastmail.fm>
2019-05-21 22:33:02 +0100

    Stub URL.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const url = require('url');
@@ -7,46 +6,49 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', function (req, res) {
+    res.render('index');
 });
 
 app.get('/render', function (req, res) {
-  const request = require('request');
-  const N3 = require('n3');
-  const parser = new N3.Parser({ format: 'turtle' });
-  const store = new N3.Store();
-  const ttlurl = req.query.url;
-  
-  request(ttlurl, function (error, response, body) {    
-    
-    store.addQuads(parser.parse(body));
+    const request = require('request');
+    const N3 = require('n3');
+    const parser = new N3.Parser({format: 'turtle'});
+    const store = new N3.Store();
+    const urlstub = req.query.url;
+    const ttlurl = `https://raw.githubusercontent.com/ukparliament/ontologies/master/${urlstub}/${urlstub}-ontology.ttl`;
 
-    const subjects = store.getSubjects();
-    
-    var subjectPacks = [];
-        
-    subjects.forEach(function(subject) {
-        var subjectPack = [];
-        store.forEach(
-          function(thisQuad){subjectPack.push(thisQuad);}, subject.id, null, null, null
-        );
-        subjectPacks.push(subjectPack);
-      });
-                
-    
-    res.render('render', { 
-        subjects: subjects,
-        subjectPacks: subjectPacks,
-        predicates: store.getPredicates().map(predicate => predicate.id),
-        objects: store.getObjects(),
-        graphs: store.getGraphs()
+    request(ttlurl, function (error, response, body) {
+
+        store.addQuads(parser.parse(body));
+
+        const subjects = store.getSubjects();
+
+        var subjectPacks = [];
+
+        subjects.forEach(function (subject) {
+            var subjectPack = [];
+            store.forEach(
+                function (thisQuad) {
+                    subjectPack.push(thisQuad);
+                }, subject.id, null, null, null
+            );
+            subjectPacks.push(subjectPack);
         });
-    
+
+
+        res.render('render', {
+            subjects: subjects,
+            subjectPacks: subjectPacks,
+            predicates: store.getPredicates().map(predicate => predicate.id),
+            objects: store.getObjects(),
+            graphs: store.getGraphs()
+        });
+
     });
 });
-            
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Up: port ' + listener.address().port);
+
+const listener = app.listen(process.env.PORT, function () {
+    console.log('Up: port ' + listener.address().port);
 });
